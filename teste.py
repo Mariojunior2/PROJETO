4a5a61386cd85435ce3b83536308e820561b2ccf
Mariojunior2 <mario.junior2@alunos.sc.senac.br>
2024-09-09 19:45:17 -0300

    treino

--- teste.py
+++ teste.py
@@ -13,10 +13,12 @@ def pessoas_equipe(pessoa):
     else:
         print(f"Pessoa nao achada: '{pessoa}'")
         return None
-    
+    ## Todo o Processo acima seria caso desse errado onde ele consulta e da o errado ou certo
     
 if __name__ == "__main__":
     nome = input(f"digite o nome da pessoa: ")
     resultado = pessoas_equipe(nome)
     if resultado:
         print(f"encontrado! '{resultado}'")
+        
+## E aqui seria se o processo desse certo 
