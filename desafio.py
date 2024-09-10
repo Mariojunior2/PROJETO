31220be2e85f6d8121b47e91f867e8e54f1c3705
Mariojunior2 <mario.junior2@alunos.sc.senac.br>
2024-09-10 20:24:28 -0300

    y7

--- desafio.py
+++ desafio.py
@@ -5,9 +5,24 @@ from difflib import SequenceMatcher  # Para comparar similaridade entre resposta
 
 
 quiz = {
-    "":"",
-    "":"",
-    "":"",
-    "":"",
-    "":""
+    "Qual a capital do Brasil?":"brasilia",
+    "Nome do Criador do Jogo":"mario",
+    "O presidente atual":"lula",
+    "O melhor amigo do patrick":"bob esponja",
+    "League of Legends é?":"ruim"
 }
+
+
+primeira_pergunta, primeira_resposta = next(iter(quiz.items()))
+print("Pergunta:", primeira_pergunta)
+pergunta = input('responda! ')
+pergunta = pergunta.lower()
+if pergunta in primeira_resposta:
+    print('Acertou')
+else: 
+    print('errou trouxa')
+   
+
+
+
+
