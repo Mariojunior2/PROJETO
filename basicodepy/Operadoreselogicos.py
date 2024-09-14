63d5a807f5e424b24c951b15392d10d9a9f9f7c2
Mariojunior2 <mario.junior2@alunos.sc.senac.br>
2024-09-14 02:52:04 -0300

    a

---
+++ basicodepy/Operadoreselogicos.py
@@ -0,0 +1,15 @@
+# Metod seria Metodo
+
+soma = 10 + 5 # Resultado 15 Metod Soma
+
+diferenca = 10 - 5 # Resultado 5 Metod Diminuir
+
+produto = 10 * 5 # Resultado 50 Metod Multiplicar
+
+quociente = 10 / 5 # Resultado 2.0 divisao
+
+potencia = 2 ** 3 # Resultado 8 Metod Elevado
+
+# Maior que (>), Menor que (<), Igual a (==), Diferente de (!=), Maior ou igual a (>=), Menor ou igual a (<=).
+
+
